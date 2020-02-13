86f19dbc395aec870878d5f6463b12f338a6b468
Suman Pathak <50736574+sumanpathak14@users.noreply.github.com>
2020-02-13 18:04:02 +0530

    Create vsearch.py

---
+++ vsearch.py
@@ -0,0 +1,11 @@
+
+
+def search4vowels(word: str) -> set:
+    """Return any vowels found in a supplied word."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(word))
+
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+    """Return a set of the 'letters' found in 'phrase'."""
+    return set(letters).intersection(set(phrase))
