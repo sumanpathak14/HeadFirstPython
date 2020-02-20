ddc1b26988df20ba4e1bd79db3bed199ae4b9a28
Suman Pathak <50736574+sumanpathak14@users.noreply.github.com>
2020-02-20 11:15:33 +0530

    Create vsearch4web_v2.py

---
+++ vsearch4web_v2.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import vsearch
+
+app = Flask(__name__)
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(vsearch.search4letters(phrase,letters))
+    return render_template('results.html',the_title=title,the_results=results,the_phrase=phrase,the_letters=letters)
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+app.run(debug=True)
